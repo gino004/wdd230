b25be9bef399e09c4959733c1c95adabd0753564
gino004 <gino2137@hotmail.com>
2024-02-06 23:04:28 -0500

    hechos

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,8 +1,3 @@
-const d = new Date();
-// Get year
-const year = d.getFullYear();
-document.querySelector('#current-year').textContent = year;
-
 // Get last updated date
 const lastUpdate = document.lastModified
 document.querySelector('#last-updated').textContent = lastUpdate;
