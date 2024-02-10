26d72b60c7c087aee8ed0c3298c9f3e42fe331a2
gino004 <gino2137@hotmail.com>
2024-02-10 18:29:59 -0500

    Dom Javascript

---
+++ lesson05/scripts/bom.js
@@ -0,0 +1,25 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('#list');
+
+button.addEventListener('click', () => {
+
+    if (input.value != '') {
+        const li = document.createElement('li');
+        const deletebutton = document.createElement('button');
+
+        li.textContent = input.value;
+        deletebutton.textContent = '❌';
+
+        li.append(deletebutton);
+        list.append(li);
+
+        deletebutton.addEventListener('click', function() {
+            list.removeChild(li);
+            input.focus();
+
+        });
+        input.value = '';
+        input.focus();
+    }
+});
