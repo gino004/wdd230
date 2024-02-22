6dd11343c5fb7d6d079d07ba945cbe49c359b8e0
gino004 <gino2137@hotmail.com>
2024-02-22 18:24:30 -0500

    vvvv

--- chamber/scripts/windchill.js
+++ chamber/scripts/windchill.js
@@ -1,5 +1,4 @@
 const calcularFactorSensacionTermica = (temperaturaFahrenheit, velocidadVientoMPH) => {
-
     if (temperaturaFahrenheit <= 50 && velocidadVientoMPH > 3.0) {
         const Twc = 35.74 + 0.6215 * temperaturaFahrenheit - 35.75 * Math.pow(velocidadVientoMPH, 0.16) + 0.4275 * temperaturaFahrenheit * Math.pow(velocidadVientoMPH, 0.16);
         return Twc.toFixed(2) + " °F";
@@ -7,12 +6,8 @@ const calcularFactorSensacionTermica = (temperaturaFahrenheit, velocidadVientoMP
         return "N/A";
     }
 }
-
-
 const temperaturaFahrenheit = parseFloat(document.getElementById('temperature').innerText);
 const velocidadVientoMPH = parseFloat(document.getElementById('windspeed').innerText);
 
-
 const factorSensacionTermica = calcularFactorSensacionTermica(temperaturaFahrenheit, velocidadVientoMPH);
-
 document.getElementById('windchill').innerText = factorSensacionTermica;
