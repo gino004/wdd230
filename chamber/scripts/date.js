cc6488cb8e9d045a2c0dac975922cf97361d82c2
gino004 <gino2137@hotmail.com>
2024-03-13 22:06:09 -0500

    formulario tarea

---
+++ chamber/scripts/date.js
@@ -0,0 +1,5 @@
+ document.addEventListener('DOMContentLoaded', function() {
+     var date = Date.now();
+     var campoFechaHora = document.getElementById('date');
+     campoFechaHora.value = date;
+ });
