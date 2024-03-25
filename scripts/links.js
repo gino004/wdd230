0a498220b480b07ba510943da46b9cc0024a0874
gino004 <gino2137@hotmail.com>
2024-03-25 13:45:03 -0500

    arreglo de enlaces

--- scripts/links.js
+++ scripts/links.js
@@ -11,26 +11,6 @@ async function getLinks() {
     }
 }
 
-// function displayLinks(data) {
-//     const main = document.querySelector('main');
-//     data.lessons.forEach(lesson => {
-//         const weekName = `Week ${lesson.lesson}`;
-//         const linksList = document.createElement('ul');
-//         lesson.links.forEach(link => {
-//             const listItem = document.createElement('li');
-//             const anchor = document.createElement('a');
-//             anchor.href = `${baseURL}${link.url}`;
-//             anchor.textContent = link.title;
-//             listItem.appendChild(anchor);
-//             linksList.appendChild(listItem);
-//         });
-//         const weekHeader = document.createElement('h3');
-//         weekHeader.textContent = weekName;
-//         main.appendChild(weekHeader);
-//         main.appendChild(linksList);
-//     });
-// }
-
 function displayLinks(data) {
     const main = document.querySelector('main');
     const card01 = main.querySelector('.card01');
@@ -38,9 +18,8 @@ function displayLinks(data) {
     // Elimina solo los enlaces dentro de card01
     card01.innerHTML = '';
 
+    const linksList = document.createElement('ul');
     data.lessons.forEach(lesson => {
-        const weekName = `Week ${lesson.lesson}`;
-        const linksList = document.createElement('ul');
         lesson.links.forEach(link => {
             const listItem = document.createElement('li');
             const anchor = document.createElement('a');
@@ -49,11 +28,16 @@ function displayLinks(data) {
             listItem.appendChild(anchor);
             linksList.appendChild(listItem);
         });
-        const weekHeader = document.createElement('h3');
-        weekHeader.textContent = weekName;
-        card01.appendChild(weekHeader);
-        card01.appendChild(linksList);
     });
+
+    // Agrega el título "Learning Activities"
+    const activitiesTitle = document.createElement('h3');
+    activitiesTitle.textContent = "Learning Activities";
+    card01.appendChild(activitiesTitle);
+
+    // Agrega los enlaces dentro de card01
+    card01.appendChild(linksList);
 }
+
 
 getLinks();
