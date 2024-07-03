a88e695c225d9c6c7b842022ce2e6fe344a948fe
gino004 <gino2137@hotmail.com>
2024-07-02 22:14:48 -0500

    Fixed Up

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,14 +1,40 @@
+// const modeButton = document.querySelector("#mode");
+// const main = document.querySelector("main");
+
+// modeButton.addEventListener("click", () => {
+//     if (modeButton.textContent.includes("🌑")) {
+//         main.style.background = "#000";
+//         main.style.color = "#fff";
+//         modeButton.textContent = "☀️";
+//     } else {
+//         main.style.background = "#eee";
+//         main.style.color = "#000";
+//         modeButton.textContent = "🌑";
+//     }
+// });
+
+
 const modeButton = document.querySelector("#mode");
 const main = document.querySelector("main");
 
 modeButton.addEventListener("click", () => {
-    if (modeButton.textContent.includes("🕶️")) {
+    if (modeButton.textContent.includes("🌑")) {
         main.style.background = "#000";
         main.style.color = "#fff";
-        modeButton.textContent = "🌞";
+        modeButton.classList.remove("transition-out");
+        modeButton.classList.add("transition-in");
+        setTimeout(() => {
+            modeButton.textContent = "🌞";
+            modeButton.classList.remove("transition-in");
+        }, 250);
     } else {
         main.style.background = "#eee";
         main.style.color = "#000";
-        modeButton.textContent = "🕶️";
+        modeButton.classList.remove("transition-out");
+        modeButton.classList.add("transition-in");
+        setTimeout(() => {
+            modeButton.textContent = "🌑";
+            modeButton.classList.remove("transition-in");
+        }, 250);
     }
 });
