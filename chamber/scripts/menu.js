dbe500db2fd2b994e3b9aad895800453e181465f
gino004 <gino2137@hotmail.com>
2024-07-13 20:08:51 -0500

    Fixed Up

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,17 +1,3 @@
-// const gridbutton = document.querySelector("#grid");
-// const listbutton = document.querySelector("#list");
-// const display = document.querySelector(".negocios");
-
-// gridbutton.addEventListener("click", () => {
-//     display.classList.add("grid");
-//     display.classList.remove("list");
-// });
-
-// listbutton.addEventListener("click", () => {
-//     display.classList.add("list");
-//     display.classList.remove("grid");
-// });
-
 const fullInfoButton = document.querySelector("#fullInfo");
 const summaryButton = document.querySelector("#summary");
 const mainContainer = document.querySelector(".negocios");
